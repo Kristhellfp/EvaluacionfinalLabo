89c7e6fc2cb2e5776e901a2fcef0b18d7c984f7e
Kristhel <klfpaz@scl.edu.gt>
2024-10-10 11:57:06 -0600

    Se agregan cambios a C2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { componente1 } from './componente/componente1/componente1.js';
-
+import { componente2 } from './componente/componente2/componente2.js';
 
 document.body.appendChild(componente1);
+document.body.appendChild(componente2);
